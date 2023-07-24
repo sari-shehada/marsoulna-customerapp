d1824833505823796cf701162340861650243b55
Sari Shehada <sarishehada@gmail.com>
2023-07-24 23:33:44 +0300

    Test5

--- lib/config/images/image_keys.dart
+++
@@ -1,9 +0,0 @@
-import 'package:marsouly/config/images/image_handler.dart';
-
-class SharedImageKey {
-  static const appIcon = '${ImageHandler.sharedImageFullPath}appIcon.png';
-}
-
-enum ThemedImageKey {
-  test,
-}
