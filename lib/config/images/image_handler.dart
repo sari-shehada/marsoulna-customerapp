5ccc19b7151c2c5715857ca7a73b61f25256bf9c
Sari Shehada <sarishehada@gmail.com>
2023-07-24 23:41:53 +0300

    Themed Images

--- lib/config/images/image_handler.dart
+++ lib/config/images/image_handler.dart
@@ -1,20 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:marsouly/config/images/themed_image_keys.dart';
 part 'themed_image_mapper.dart';
 
 class ImageHandler {
   static const String assetsPath = 'assets/';
-  static const String sharedPath = 'shared/';
   static const String darkModePath = '${assetsPath}dark/';
   static const String lightModePath = '${assetsPath}light/';
+  static const String sharedImageFullPath = '${assetsPath}shared';
 
-  static const String sharedImageFullPath = '$assetsPath$sharedPath';
-  static String get notFoundImage => '${sharedImageFullPath}notFound.png';
+  static const String notFoundImage = '${sharedImageFullPath}notFound.png';
 
-  String getThemedImagePath(ThemedImageKey key) {
+  String getThemedImagePath(ThemedImageKey key, BuildContext context) {
     String? themedImageName = _themedImageToPath[key];
     if (themedImageName == null) {
       return notFoundImage;
     }
-    return sharedImageFullPath + (_themedImageToPath[key]!);
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    return (isDarkMode ? darkModePath : lightModePath) +
+        (_themedImageToPath[key]!);
   }
 }
