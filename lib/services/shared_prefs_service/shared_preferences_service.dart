0453dcfd4316cef530fdfb5db202a1eb1d6557f4
Ismail Tayeb <tayebismail099@gmail.com>
2023-07-27 20:59:19 +0300

    Added Generic Data Reader To Shared Prefs

--- lib/services/shared_prefs_service/shared_preferences_service.dart
+++ lib/services/shared_prefs_service/shared_preferences_service.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesService {
@@ -19,6 +21,31 @@ class SharedPreferencesService {
 
   Future<void> setInt(SharedPrefsKey key, int value) async {
     await prefs.setInt(key.toString(), value);
+  }
+
+  Future<bool> writeItem<T>({required Enum key, required T value}) async {
+    try {
+      if (value is bool) {
+        return await prefs.setBool(key.toString(), value);
+      }
+      if (value is String) {
+        return await prefs.setString(key.toString(), value);
+      }
+      if (value is int) {
+        return await prefs.setInt(key.toString(), value);
+      }
+      if (value is double) {
+        return await prefs.setDouble(key.toString(), value);
+      }
+      if (value is List<String>) {
+        return await prefs.setStringList(key.toString(), value);
+      }
+      throw Exception(
+          'Exception Occurred When Calling SharedPreferencesService -> writeItem() -> Data Type Not Supported');
+    } catch (e) {
+      log(e.toString());
+      return false;
+    }
   }
 }
 
