d7f289db03a24a2b28f8845088b24ba6779385ab
Ismail Tayeb <tayebismail099@gmail.com>
2023-07-27 22:27:12 +0300

    Added HttpOverrides to GlobalConfig

--- lib/config/global/global_config.dart
+++ lib/config/global/global_config.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:marsouly/config/global/network_service_config.dart';
 
 import '../../services/shared_prefs_service/shared_preferences_service.dart';
 import '../../services/theming_service/theming_service.dart';
+import '../dev_env/http_certificates_overrides.dart';
 
 class GlobalConfig {
   static bool _alreadyInitialized = false;
@@ -13,6 +16,8 @@ class GlobalConfig {
     await SharedPreferencesService.init();
     ThemingService.init();
     NetworkServiceConfig.init(NetworkServiceBaseURLEnvironment.production);
+    //TODO: Comment HTTP Overrides if not needed
+    HttpOverrides.global = HttpCertificateOverrides();
     _alreadyInitialized = true;
   }
 }
