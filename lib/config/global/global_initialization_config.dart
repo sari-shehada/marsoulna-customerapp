5c211f3805e32b4f1748f0343f80ddb0775c3fdf
Sari Shehada <sarishehada@gmail.com>
2023-07-27 20:43:27 +0300

    Modified this file

--- lib/config/global/global_initialization_config.dart
+++ lib/config/global/global_initialization_config.dart
@@ -1,1 +1,9 @@
+class GlobalInitializationConfig {
+  static bool _alreadyInitialized = false;
 
+  static Future<void> performInitialConfiguration() async {
+    if (_alreadyInitialized) return;
+    //TODO: Add initialization here
+    _alreadyInitialized = true;
+  }
+}
