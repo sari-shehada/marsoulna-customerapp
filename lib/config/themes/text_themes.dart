c99a6e9019bd135f36ae868df2a276ac226f0990
Sari Shehada <sarishehada@gmail.com>
2023-07-31 18:47:43 +0300

    Added Text Themes to Themes Config

---
+++ lib/config/themes/text_themes.dart
@@ -0,0 +1,91 @@
+part of 'themes.dart';
+
+TextStyle get _displayLarge => TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 57.sp,
+      letterSpacing: 0.sp,
+    );
+
+TextStyle get _displayMedium => TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 45.sp,
+      letterSpacing: 0.sp,
+    );
+
+TextStyle get _displaySmall => TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 36.sp,
+      letterSpacing: 0.sp,
+    );
+
+TextStyle get _headlineLarge => TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 32.sp,
+      letterSpacing: 0.sp,
+    );
+
+TextStyle get _headlineMedium => TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 28.sp,
+      letterSpacing: 0.sp,
+    );
+
+TextStyle get _headlineSmall => TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 24.sp,
+      letterSpacing: 0.sp,
+    );
+
+TextStyle get _titleLarge => TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 22.sp,
+      letterSpacing: 0.sp,
+    );
+
+TextStyle get _titleMedium => TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 16.sp,
+      letterSpacing: 0.15.sp,
+    );
+
+TextStyle get _titleSmall => TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 14.sp,
+      letterSpacing: 0.1.sp,
+    );
+
+TextStyle get _labelLarge => TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 14.sp,
+      letterSpacing: 0.1.sp,
+    );
+
+TextStyle get _labelMedium => TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 12.sp,
+      letterSpacing: 0.5.sp,
+    );
+
+TextStyle get _labelSmall => TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 11.sp,
+      letterSpacing: 0.5.sp,
+    );
+
+TextStyle get _bodyLarge => TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 16.sp,
+      letterSpacing: 0.15.sp,
+    );
+
+TextStyle get _bodyMedium => TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 14.sp,
+      letterSpacing: 0.25.sp,
+    );
+
+TextStyle get _bodySmall => TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 12.sp,
+      letterSpacing: 0.4.sp,
+    );
--- lib/config/themes/themes.dart
+++ lib/config/themes/themes.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 part 'dark_theme.dart';
 part 'light_theme.dart';
+part 'text_themes.dart';
 
-final ThemeData _sharedTheme = ThemeData(useMaterial3: true);
+final ThemeData _sharedTheme = ThemeData(
+  fontFamily: 'Marsoulna',
+  useMaterial3: true,
+  textTheme: TextTheme(
+    displayLarge: _displayLarge,
+    displayMedium: _displayMedium,
+    displaySmall: _displaySmall,
+    headlineLarge: _headlineLarge,
+    headlineMedium: _headlineMedium,
+    headlineSmall: _headlineSmall,
+    titleLarge: _titleLarge,
+    titleMedium: _titleMedium,
+    titleSmall: _titleSmall,
+    labelLarge: _labelLarge,
+    labelMedium: _labelMedium,
+    labelSmall: _labelSmall,
+    bodyLarge: _bodyLarge,
+    bodyMedium: _bodyMedium,
+    bodySmall: _bodySmall,
+  ),
+);
