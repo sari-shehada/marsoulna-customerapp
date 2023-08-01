05890e23488fc82df823869e19f949efb430d81a
Ismail Tayeb <tayebismail099@gmail.com>
2023-08-01 22:06:13 +0300

    Added UniLiks to print links

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:marsouly/services/locale_service/locales_service.dart';
+import 'package:uni_links/uni_links.dart';
 
 import 'app/routes/app_pages.dart';
 import 'config/design/design_config.dart';
@@ -12,6 +13,7 @@ import 'services/theming_service/theming_service.dart';
 
 Future<void> main() async {
   await GlobalConfig.performInitialConfiguration();
+  await Routing.initUniLinks();
   runApp(const Wrapper());
 }
 
@@ -36,8 +38,49 @@ class Wrapper extends StatelessWidget {
           darkTheme: darkTheme,
           translationsKeys: AppTranslation.translations,
           debugShowCheckedModeBanner: false,
+          routingCallback: (routing) {
+            print('Called ');
+            if (routing == null) {
+              return;
+            }
+            print(routing.current);
+            // if (routing.route!.isFirst) {
+            //   // The app was launched from a deep link
+            //   final uri = routing.route.settings;
+            //   if (uri.path == '/product') {
+            //     final productId = uri.queryParameters['id'];
+            //     // Navigate to the product page with the given ID
+            //     Get.toNamed('/product/$productId');
+            //   }
+            // }
+          },
         );
       },
     );
+  }
+}
+
+class Routing {
+  static Future<void> initUniLinks() async {
+    try {
+      final initialLink = await getInitialUri();
+      _handleLink(initialLink);
+    } on Exception catch (e) {
+      print('Error initializing uni_links: $e');
+    }
+
+    uriLinkStream.listen((Uri? link) {
+      print('Path Was:${link?.path}');
+      _handleLink(link);
+    });
+  }
+
+  static void _handleLink(Uri? link) {
+    if (link != null) {
+      final path = link.path;
+      if (path == Routes.LOADER) {
+        Get.toNamed(Routes.LOADER);
+      }
+    }
   }
 }
