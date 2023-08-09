78ed4763e7099f35107507dd71018c671da5d080
Sari Shehada <sarishehada@gmail.com>
2023-08-09 16:21:32 +0300

    Decreased size, added shadow and adjusted the next icon to be aligned in the middle

--- lib/app/modules/onboarding/views/widgets/onboarding_next_button_widget.dart
+++ lib/app/modules/onboarding/views/widgets/onboarding_next_button_widget.dart
@@ -15,7 +15,7 @@ class OnboardingNextButtonWidget extends GetView<OnboardingTabViewController> {
     ThemeData themeData = Theme.of(context);
     ColorScheme colorScheme = themeData.colorScheme;
     return SizedBox.square(
-      dimension: 115.responsiveFromTextSize,
+      dimension: 95.responsiveFromTextSize,
       child: Stack(
         fit: StackFit.expand,
         alignment: Alignment.center,
@@ -37,15 +37,26 @@ class OnboardingNextButtonWidget extends GetView<OnboardingTabViewController> {
             decoration: BoxDecoration(
               color: colorScheme.primary,
               shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 20,
+                  color: colorScheme.shadow.withOpacity(.1),
+                ),
+              ],
             ),
             clipBehavior: Clip.hardEdge,
             child: Material(
               color: Colors.transparent,
               child: InkWell(
                 onTap: () => Get.find<OnboardingController>().next(),
-                child: Icon(
-                  Icons.arrow_forward_ios,
-                  color: colorScheme.surface,
+                child: Padding(
+                  padding: EdgeInsetsDirectional.only(
+                    start: 2.5.responsiveFromWidth,
+                  ),
+                  child: Icon(
+                    Icons.arrow_forward_ios,
+                    color: colorScheme.surface,
+                  ),
                 ),
               ),
             ),
